afcaca9e8b452c09c00eaeb45dcc21ab0fc08152
fdrtec <fdrtec@gmail.com>
2023-03-04 22:09:50 -0300

    anotações de validações

--- src/main/java/com/fdrtec/api/repair/tips/domain/model/dto/ProductDto.java
+++ src/main/java/com/fdrtec/api/repair/tips/domain/model/dto/ProductDto.java
@@ -4,11 +4,10 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-import org.hibernate.annotations.CreationTimestamp;
-
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,17 +21,21 @@ public class ProductDto implements Serializable {
 
 	UUID id;    
     
-    @NotBlank    
+    @NotBlank 
+    @Size(max = 100)   
     String productName;    
     
     @NotBlank
+    @Size(max = 100) 
     String productNumber; 
     
     @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
     LocalDateTime createDate;
 }
 
-    //@NotNull
     // @NotBlank
+    // @NotNull
+    // @NotEmpty
     // @Email
+    // @Size(min = 5, max =100)
     // @Pattern(regexp = "\\d{4,6}")
