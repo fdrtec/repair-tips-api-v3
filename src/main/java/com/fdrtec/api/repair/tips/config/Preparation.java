88ccf341558fb1c3077e4d5bc6f70275e023383d
fdrtec <fdrtec@gmail.com>
2023-03-04 22:47:13 -0300

    Profile como component

--- src/main/java/com/fdrtec/api/repair/tips/config/Preparation.java
+++ src/main/java/com/fdrtec/api/repair/tips/config/Preparation.java
@@ -1,7 +1,9 @@
 package com.fdrtec.api.repair.tips.config;
 import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
 import org.springframework.boot.CommandLineRunner;
 
+@Component
 @Profile(value = "dev")
 public class Preparation implements CommandLineRunner {
 
